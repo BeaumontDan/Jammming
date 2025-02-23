763bc3c5b6c6d8f7482247caf585ee8dda22c653
Dan Beaumont <beaumont.dan@gmail.com>
2025-02-23 11:12:13 +0000

    Update Spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,7 +1,7 @@
 let accessToken = "";
 
 const clientID = "520c3806fd5f41bf90fcddc4db84db6a";
-const redirectUrl = "https://jammming-tracks.netlify.app/callback";
+const redirectUrl = "https://jammming-tracks.netlify.app/";
 
 const Spotify = {
   getAccessToken() {

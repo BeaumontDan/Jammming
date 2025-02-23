201d9f30dd956c62329fbfa1b270265d6533f7ec
Dan Beaumont <beaumont.dan@gmail.com>
2025-02-23 17:36:35 +0000

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className={styles.Footer}>
       <p>
-        <a className={styles.footerLink} href="https://codecademy.com" title="Codecademy Homepage">Codecademy</a> project by <a className={styles.footerLink} href="https://github.com/BeaumontDan" title="GitHub Profile">Dan Beaumont&colon;</a>
+        <a className={styles.footerLink} href="https://codecademy.com" title="Codecademy Homepage">Codecademy</a> project by <a className={styles.footerLink} href="https://github.com/BeaumontDan" title="GitHub Profile">Dan Beaumont</a>
       </p>
       <p>
         <a className={styles.footerLink} href="https://github.com/BeaumontDan/jammming" title="Code Solution">Spotify Palylist React App</a> 
